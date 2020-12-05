82e6ebb3df0ed909980eb947fe94874872676e19
omur8819 <omur8819@gmail.com>
2020-12-05 22:24:17 +0300

    Project updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello world</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Take dogs for a walk</li>
+        <li>Take the rubbish out</li>
+      </ul>
+      
     </div>
   );
 }
